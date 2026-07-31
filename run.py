0e96ab096a728a391341bfffc373aa786d828dec
helloworld_21 <13418168728@163.com>
2026-07-31 09:28:43 +0800

    算法改进 添加力度桶等机制

---
+++ run.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import sys
+
+
+sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
+
+from jumpjump_autoclicker.__main__ import main  # noqa: E402
+
+
+if __name__ == "__main__":
+    main()
